92a2b94fd6e790548c3c1c1eff770cc45f55dfd1
Emilia <e.lubanska@gmail.com>
2020-12-05 18:12:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,27 @@ app = Flask(__name__)
 
 # 3. Define static routes
 @app.route("/")
-def index():
-    return "Hello, world!"
+def welcome():
+    return (
+        f"Welcome to Hawaii weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
 
 
-@app.route("/about")
-def about():
-    name = "Peleke"
-    location = "Tien Shan"
+@app.route("/api/v1.0/precipitation")
+def prcp():
+        return f"API: /api/v1.0/precipitation"
 
-    return f"My name is {name}, and I live in {location}."
+@app.route("/api/v1.0/stations")
+def station():
+        return f"API: /api/v1.0/stations"
 
-
-
+@app.route("/api/v1.0/tobs")
+def tobs():
+        return f"API: /api/v1.0/tobs"
 
 
 if __name__ == "__main__":
